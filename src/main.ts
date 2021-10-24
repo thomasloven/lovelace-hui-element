f55aa50796163c73784ce816ffdb3a1ab0b45cb0
Thomas Lovén <thomasloven@gmail.com>
2021-10-24 13:31:37 +0000

    Simplifications

--- src/main.ts
+++ src/main.ts
@@ -1,86 +1,66 @@
-import { LitElement, html } from "card-tools/src/lit-element";
-import {
-  createCard,
-  createElement,
-  createEntityRow,
-} from "card-tools/src/lovelace-element";
 import pjson from "../package.json";
 
-class HuiElement extends LitElement {
+class HuiElement extends HTMLElement {
   element;
+  _hass;
 
-  setConfig(config) {
+  async setConfig(config) {
     const conf = JSON.parse(JSON.stringify(config));
+    const helpers = await (window as any).loadCardHelpers();
+
     let type;
     if (conf.card_type !== undefined) type = "card";
-    if (conf.element_type !== undefined) {
-      if (type !== undefined) {
-        this.element = createCard({
-          type: "error",
-          error:
-            "Must specify only one of card_type, element_type or row_type.",
-          origConfig: conf,
-        });
-        return;
-      }
-      type = "element";
-    }
-    if (conf.row_type !== undefined) {
-      if (type !== undefined) {
-        this.element = createCard({
-          type: "error",
-          error:
-            "Must specify only one of card_type, element_type or row_type.",
-          origConfig: conf,
-        });
-        return;
-      }
-      type = "row";
-    }
+    if (conf.element_type !== undefined) type = type ? "multiple" : "element";
+    if (conf.row_type !== undefined) type = type ? "multiple" : "row";
+
     switch (type) {
       case "card":
         conf.type = conf.card_type;
         delete conf.card_type;
-        this.element = createCard(conf);
+        this.element = await helpers.createCardElement(conf);
         break;
+
       case "element":
         conf.type = conf.element_type;
         delete conf.element_type;
-        this.element = createElement(conf);
+        this.element = await helpers.createHuiElement(conf);
         break;
+
       case "row":
         conf.type = conf.row_type;
         if (conf.type === "default") conf.type = undefined;
         delete conf.row_type;
-        this.element = createEntityRow(conf);
+        this.element = await helpers.createRowElement(conf);
         break;
+
+      case "multiple":
+        this.element = await helpers.createCardElement({
+          type: "error",
+          error: "Must specify only one of card_type, element_type or row_type",
+          origConfig: conf,
+        });
+        break;
+
       default:
-        this.element = createCard({
+        this.element = await helpers.createCardElement({
           type: "error",
           error: "Must specify card_type, element_type or row_type.",
           origConfig: conf,
         });
     }
+
+    while (this.firstChild) this.removeChild(this.firstChild);
+    this.appendChild(this.element);
+    this.element.hass = this._hass;
   }
 
   set hass(hass) {
-    this.element.hass = hass;
-  }
-
-  createRenderRoot() {
-    return this;
+    this._hass = hass;
+    if (this.element) this.element.hass = hass;
   }
 
   get shadowRoot() {
-    return this.element.shadowRoot;
-  }
-
-  // get modElement() {
-  //   return this.element;
-  // }
-
-  render() {
-    return html`${this.element}`;
+    return this.element?.shadowRoot;
   }
 }
 
